072df0b860cab5a3968377175b8e0f629f63367d
Alexander Lall <alexander.heddier@gmx.de>
2022-06-09 15:28:38 +0530

    initial commit

---
+++ src/components/slider/slider.component.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
+import './slider.styles.scss';
+
+const Slider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const length = slides.length;
+
+  const prevSlide = () => {
+    setCurrentIndex(currentIndex === 0 ? length - 1 : currentIndex - 1);
+  }
+
+  const nextSlide = () => {
+    setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1);
+  }
+
+  console.log(currentIndex);
+
+  if(!Array.isArray(slides) || slides.length === 0) {
+      return null;
+  }
+
+  return (
+    <section className='slider'>
+      <FaArrowAltCircleLeft className='arrow left-arrow' onClick={prevSlide}/>
+      <FaArrowAltCircleRight className='arrow right-arrow' onClick={nextSlide}/>
+      {slides.map((elm, idx) => {
+        return (
+            <div key={idx} className={`slide ${currentIndex === idx ? 'active' : ''}`}>
+                {currentIndex === idx && (
+                    <img className="image" src={elm.image} alt='Slideshow Image' />
+                )}
+            </div>
+        ); 
+      })}
+    </section>
+  );
+};
+
+export default Slider;
